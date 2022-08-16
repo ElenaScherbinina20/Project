450864d9a8712ad59b51783159a6ac00553eb240
ElenaScherbinina20 <59705232+ElenaScherbinina20@users.noreply.github.com>
2022-08-16 13:53:20 +0600

    Добавила заголовок headers в аргумент функции

--- test_partners_add.py
+++ test_partners_add.py
@@ -4,10 +4,8 @@ base_url = "https://payment.test.paymatrix.ru"  # базовая url
 
 post_resource = "/api/partners/add"  # Ресурс метода POST
 
-content_type = "application/json"
-
 json_for_create_new_connection = {
-             "name": "salmon_111113321133313133111111",
+             "name": "salmon_1111133211333131331111111",
              "gateway": "testTerminal",
              "limit": 0,
              "token": {
@@ -18,16 +16,13 @@ json_for_create_new_connection = {
                  "signature": "9f18ebc1a920aee99b500a76041abbab946bddd8148d72a3b712b3971ffeb1e286dee53f233b50f575cbfef62d7fbde8"
              }
      }
+headers = {'Content-Type': 'application/json'}
 
-def test_create_new_connection(base_url, post_resource, content_type, json_for_create_new_connection):
+def test_create_new_connection(base_url, post_resource, headers, json_for_create_new_connection):
 
     post_url = base_url + post_resource
     print("URL: " + post_url)
-
-    print("Content-type: " + content_type)
-
-    result_post = requests.post(post_url, json = json_for_create_new_connection)
+    result_post = requests.post(post_url, json=json_for_create_new_connection, headers = headers)
     print(result_post.text)
 
-
-test_create_new_connection(base_url, post_resource, content_type, json_for_create_new_connection)
+test_create_new_connection(base_url, post_resource, headers, json_for_create_new_connection)
