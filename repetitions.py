55c34006f9ef1bc0413cbac72610b9df95fb4dc9
ElenaScherbinina20 <59705232+ElenaScherbinina20@users.noreply.github.com>
2022-08-23 02:50:49 +0600

    Update repetitions.py

--- repetitions.py
+++ repetitions.py
@@ -1,16 +1,14 @@
 import math
 
-list_e = ["Number", "repetition"]
+list_e = ["a", "11", "z2", "y3", "6444", "@@@"]
 
 def getCountStrWithoutRepetitions(list):
     count = 0
     length = len(list)
     for element in list_e:
         count += len(element)
-        n = math.factorial(count)
-        k = math.factorial(count - length)
-        f = n/k
-    return f
+        n = math.factorial(count)/(count - length)
+    return n
 
 print("Number of options without repetition: ", getCountStrWithoutRepetitions(list_e))
 
