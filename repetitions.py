c58eb7852584f41206ae3fe58680ae25f1ccfa5f
ElenaScherbinina20 <59705232+ElenaScherbinina20@users.noreply.github.com>
2022-08-26 23:48:34 +0600

    Update repetitions.py

--- repetitions.py
+++ repetitions.py
@@ -2,8 +2,10 @@ import math
 import itertools
 from itertools import *
 list_e = ["a", "1", "2", "3", "4", "@"]
+
 def getCountStrWithoutRepetitions(list_e):
     b=0
+    a = []
     count = int(len(list_e))
     while b < count:
         b += 1
@@ -14,13 +16,16 @@ def getCountStrWithoutRepetitions(list_e):
             length_str = len(single_str)
             n = math.factorial(count)
             k = math.factorial(count - length_str)
-            c = int(n / k)
-        print("Number of options without repetition: ", c)
+            s= int(n / k)
+        a.append(s)
+        d = sum(a)
+    return d
 
-getCountStrWithoutRepetitions(list_e)
+print("Number of options without repetition: ", getCountStrWithoutRepetitions(list_e))
 
 def getCountStrWithRepetitions(list_e):
     b=0
+    a = []
     count = int(len(list_e))
     while b < count:
         b += 1
@@ -29,7 +34,9 @@ def getCountStrWithRepetitions(list_e):
             delimiter = ''
             single_str = delimiter.join(i)
             length_str = len(single_str)
-            c = int(count**length_str)
-        print("Number of options with repetitions: ", c)
+            s = int(count**length_str)
+        a.append(s)
+        d = sum(a)
+    return d
 
-getCountStrWithRepetitions(list_e)
+print("Number of options with repetitions: ", getCountStrWithRepetitions(list_e))
