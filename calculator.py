4a7b2ddb838b0ea635b776ccacd6db4142867ed0
ElenaScherbinina20 <59705232+ElenaScherbinina20@users.noreply.github.com>
2022-09-12 18:19:49 +0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,48 +1,35 @@
 from fractions import Fraction
 
-class Calculator():
-    """Создаем калькулятор"""
-
-    def __init__(self):
-        pass
-        print("Дробные значения")
-
+class Fraction_1():
     def fraction_1(self):
         x1 = int(input("Введите числитель первой дроби: "))
         y1 = int(input("Введите знаменатель первой дроби: "))
         self.value1 = Fraction(x1, y1)
         print("A =", self.value1)
+        return self.value1
 
+class Fraction_2():
     def fraction_2(self):
         x2 = int(input("Введите числитель второй дроби: "))
         y2 = int(input("Введите знаменатель второй дроби: "))
         self.value2 = Fraction(x2, y2)
-        print("A =", self.value2)
+        print("B =", self.value2)
+        return self.value2
 
-    def sum_f(self):
-        """Суммируем выражение"""
-        sum =  self.value1+ self.value2
-        print("A+B =", sum)
+frac1 = Fraction_1()
+a = frac1.fraction_1()
 
-    def minus_f(self):
-        """Находим разницу"""
-        minus = self.value1 - self.value2
-        print("A-B =", minus)
+frac2 = Fraction_2()
+b = frac2.fraction_2()
 
-    def divide_f(self):
-        """Делим"""
-        divide = self.value1 / self.value2
-        print("A/B =", divide)
+sum = a + b
+print("A+B =", sum)
 
-    def multiplication_f(self):
-        """Умножаем"""
-        multiplication = self.value1 * self.value2
-        print("A*B =", multiplication)
+minus = a - b
+print("A-B =", minus)
 
-numbers = Calculator()
-numbers.fraction_1()
-numbers.fraction_2()
-numbers.sum_f()
-numbers.minus_f()
-numbers.divide_f()
-numbers.multiplication_f()
+multiplication = a * b
+print("A*B =", multiplication)
+
+divide = a / b
+print("A/B =", divide)
