facea0841949855d6c7817e475f97b561e26de76
ElenaScherbinina20 <59705232+ElenaScherbinina20@users.noreply.github.com>
2022-09-13 16:06:59 +0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,23 +5,25 @@ class Fraction_1():
         x = int(input("Введите числитель первой дроби: "))
         y = int(input("Введите знаменатель первой дроби: "))
         self.value = Fraction(x, y)
-        print("A =", self.value)
+        # print("A =", self.value)
         return self.value
 
 frac1 = Fraction_1()
 a = frac1.fraction()
+print("A =", a)
 
 frac2 = Fraction_1()
 b = frac2.fraction()
+print("B =", b)
 
 frac3 = a + b
-print("A+B =", frac3)
+print("D =", frac3)
 
 frac4 = a - b
-print("A-B =", frac4)
+print("E =", frac4)
 
 frac5 = a * b
-print("A*B =", frac5)
+print("I =", frac5)
 
 frac6 = a / b
-print("A/B =", frac6)
+print("F =", frac6)
