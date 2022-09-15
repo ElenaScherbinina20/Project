476fcea317bb2041157a1c184e531101465000e8
ElenaScherbinina20 <59705232+ElenaScherbinina20@users.noreply.github.com>
2022-09-15 18:40:34 +0600

    Update fraction.py

--- fraction.py
+++ fraction.py
@@ -1,19 +1,20 @@
-def reduce_fraction(a, b):
-    """Cокращение дробей"""
-    while a % b != 0:
-        value_1 = a
-        value_2 = b
-        a = value_2
-        b = value_1 % value_2
-    return b
-
 class Fraction:
     """Класс дробь"""
 
     def __init__(self, num, den):
         """Инициализая атрибутов объекта"""
-        self.num = num
-        self.den = den
+
+        def reduce_fraction(a, b):
+            """Cокращение дробей"""
+            while a % b != 0:
+                value_1 = a
+                value_2 = b
+                a = value_2
+                b = value_1 % value_2
+            return b
+        reduce_value = reduce_fraction(num, den)
+        self.num = num // reduce_value
+        self.den = den // reduce_value
 
     def __str__(self):
         """Возвращение строки"""
@@ -23,29 +24,29 @@ class Fraction:
         """Сумма дробей"""
         newnum = self.num * fraction.den + self.den * fraction.num
         newden = self.den * fraction.den
-        reduce_value = reduce_fraction(newnum, newden)
-        return Fraction(newnum // reduce_value, newden // reduce_value)
+
+        return Fraction(newnum, newden)
 
     def __sub__(self, fraction):
         """Разность дробей"""
         newnum = self.num * fraction.den - self.den * fraction.num
         newden = self.den * fraction.den
-        reduce_value = reduce_fraction(newnum, newden)
-        return Fraction(newnum // reduce_value, newden // reduce_value)
+
+        return Fraction(newnum, newden)
 
     def __mul__(self, fraction):
         """Произведение дробей"""
         newnum = self.num * fraction.num
         newden = self.den * fraction.den
-        reduce_value = reduce_fraction(newnum, newden)
-        return Fraction(newnum // reduce_value, newden // reduce_value)
+
+        return Fraction(newnum, newden)
 
     def __truediv__(self, fraction):
         """Деление дробей"""
         newnum = self.num * fraction.den
         newden = self.den * fraction.num
-        reduce_value = reduce_fraction(newnum, newden)
-        return Fraction(newnum // reduce_value, newden // reduce_value)
+
+        return Fraction(newnum, newden)
 
 class Exponentiate(Fraction):
     def __init__(self, num, den):
@@ -54,11 +55,11 @@ class Exponentiate(Fraction):
     def __pow__(self, other):
         newnum = self.num**other
         newden = self.den**other
-        reduce_value = reduce_fraction(newnum, newden)
-        return Exponentiate(newnum // reduce_value, newden // reduce_value)
+
+        return Exponentiate(newnum, newden)
 
 E = Exponentiate(2, 5)
-print("Дробь для возведения в степень: ", E**3)
+print("Дробь возведенна в степень: ", E**3)
 
 A = Fraction(2,5)
 print("Значение первой дроби: ", A)
@@ -66,6 +67,6 @@ print("Значение первой дроби: ", A)
 B = Fraction(3,7)
 print(f"Значение второй дроби: {B}\n"
       f"A+B= {A+B}\n"
-      f"A+B= {A-B}\n"
-      f"A+B= {A*B}\n"
+      f"A-B= {A-B}\n"
+      f"A*B= {A*B}\n"
       f"A+B= {A/B}\n")
